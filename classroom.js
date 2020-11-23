3cdb330fbca8a2cdab5c7b964f302f127fac9d25
Ben Luo <4107322+bl1428@users.noreply.github.com>
2020-11-22 19:45:20 -0600

    classroom: style top bar, add blank video panes

--- classroom.js
+++ classroom.js
@@ -1,3 +1,18 @@
+var present_students = [
+    'Adam B.',
+    'Katya B.',
+    'Lisa D.',
+    'Kristen L.',
+    'Katie O.',
+    'Haley P.',
+    'Marty R.',
+    'Josh S.',
+    'Jessie S.',
+    'Sammy T.',
+    'Billy W.',
+    'Amber Z.',
+];
+
 $(document).ready(function() {
 
 });
