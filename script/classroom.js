f94c5a887dc0aa7d817e8f830bea1ed3aafb8034
Ben Luo <4107322+bl1428@users.noreply.github.com>
2020-11-22 23:58:58 -0600

    Classroom: Add student options modal, bottom controls

--- script/classroom.js
+++ script/classroom.js
@@ -8,3 +8,13 @@ $(function () {
 });
 
 $('#confirmEndClassBtn').on('click', () => window.location.href = "index.html");
+
+$('i.video-icon.fa-ellipsis-h').on('click', function(evt) {
+    evt.preventDefault();
+    let e = $(this);
+    let modal = $('#studentOptionsModal');
+
+    let studentName = e.parent().siblings().children('.participant-name').text();
+    modal.find('.modal-title').html(studentName);
+    modal.modal('show');
+});
