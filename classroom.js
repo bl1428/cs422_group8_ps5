8f3b43fe5bf43f85dd956e43363b4c6be2194321
Ben Luo <4107322+bl1428@users.noreply.github.com>
2020-11-22 21:20:27 -0600

    classroom: add video control icons

--- classroom.js
+++ classroom.js
@@ -1,18 +1,3 @@
-var present_students = [
-    'Adam B.',
-    'Katya B.',
-    'Lisa D.',
-    'Kristen L.',
-    'Katie O.',
-    'Haley P.',
-    'Marty R.',
-    'Josh S.',
-    'Jessie S.',
-    'Sammy T.',
-    'Billy W.',
-    'Amber Z.',
-];
-
 $(document).ready(function() {
 
 });
