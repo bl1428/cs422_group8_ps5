8a0bfcc901db23b9e2bb8b87653b93eb654c2aec
Ben Luo <4107322+bl1428@users.noreply.github.com>
2020-11-23 20:18:36 -0600

    fix display of student name in options modal

--- script/classroom.js
+++ script/classroom.js
@@ -26,21 +26,21 @@ $(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip() // enable all bootstrap tooltips
 });
 
-
 $('#confirmEndClassBtn').on('click', () => window.location.href = "index.html");
 
-$('i.video-icon.fa-ellipsis-h').on('click', (evt) => {
+// show student options modal
+$('i.video-icon.fa-ellipsis-h').on('click', function(evt) {
     evt.preventDefault();
     let e = $(this);
     let modal = $('#studentOptionsModal');
 
-    let studentName = e.parent().siblings().children('.participant-name').text();
+    let studentName = e.parent().siblings().children('span').text();
     modal.find('.modal-title').html(studentName);
     modal.modal('show');
 });
 
+// link to group mode
 $('body').on('click', '#startGroupBtn', () => {
-    // window.location.href = "group_mode.html";
     let select = $('#groupModeSelect');
     switch (select.val()) {
         case '2':
